4fdb6455f2ef1919f50f41a19f0c8238b26907f1
Jack Ruder <contact@jackruder.xyz>
2024-09-01 22:57:45 -0600

    last try

--- .github/workflows/install_libtorch_macsilicon.sh
+++ .github/workflows/install_libtorch_macsilicon.sh
@@ -7,10 +7,12 @@ set -e
 # Download libtorch
 curl -L https://download.pytorch.org/libtorch/cpu/libtorch-macos-arm64-2.4.0.zip -o libtorch.zip
 
-# Unzip the libraries to /opt/libtorch
-unzip libtorch.zip -d /opt/
 
-export LIBTORCH=/opt/libtorch
+# Unzip the libraries to a writable location within the current workspace
+unzip libtorch.zip -d $(pwd)
 
 # Clean up the zip file
 rm libtorch.zip
+
+export LIBTORCH=$(pwd)/libtorch
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBTORCH/lib
