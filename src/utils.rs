b2534ccb11e1936573f72456f29f94f13d64b010
Jacob Sriraman <j.b.sriraman@gmail.com>
2026-05-04 14:10:15 -0600

    Remove tch calls from utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,5 @@
 use ndarray::{Array2, Axis};
 use polars::prelude::*;
-use tch;
 
 // This function is useful for writing functions which
 // accept pairs of List columns. Delete if unneded.
@@ -26,40 +25,3 @@ where
     }
 }
 
-pub fn array2_to_tensor<E: tch::kind::Element>(
-    array: &Array2<E>,
-    device: tch::Device,
-) -> tch::Tensor {
-    let ax0 = array.len_of(Axis(0));
-    let ax1 = array.len_of(Axis(1));
-    let tensor = tch::Tensor::from_slice(array.as_slice().unwrap())
-        .view([ax0 as i64, ax1 as i64])
-        .to_device(device);
-    tensor
-}
-
-pub fn tensor_to_array2<O: tch::kind::Element + Default>(
-    tensor: &tch::Tensor,
-    length: usize,
-    width: usize,
-) -> Array2<O> {
-    let mut dst = vec![O::default(); length * width];
-    tensor
-        .to_kind(tch::Kind::Float)
-        .to_device(tch::Device::Cpu)
-        .copy_data(&mut dst, length * width);
-    Array2::from_shape_vec((length, width), dst).unwrap()
-}
-
-pub fn tensor_to_flat<O: tch::kind::Element + Default>(
-    tensor: &tch::Tensor,
-    kind: tch::Kind,
-) -> Vec<O> {
-    let num_elems = tensor.size().into_iter().fold(1, |acc, x| acc * x) as usize;
-    let mut dst = vec![O::default(); num_elems];
-    tensor
-        .to_kind(kind)
-        .to_device(tch::Device::Cpu)
-        .copy_data(&mut dst, num_elems);
-    dst
-}
